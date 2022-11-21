9a59d04f27cc4b8fb777482124a3f9e89a3848e5
Salooma <saloomaali2000@gmail.com>
2022-11-21 15:59:21 +0530

    view option added

--- src/Main.java
+++ src/Main.java
@@ -26,30 +26,41 @@ public class Main {
 
 
     public static void main(String[] args) {
-        System.out.println("Select an option \n 1 - Add Employee \n 2 - View Employee \n 3 - Select an Employee \n" +
-                "4 - Delete an Employee \n 5 - Exit");
-        Scanner input = new Scanner(System.in);
-        int n = input.nextInt();
+        ArrayList<employee> earr = new ArrayList<employee>();
 
-        if (n == 5){
-            System.out.println("You are exiting from the program");
-        }
-        else{
+        while (true) {
+            System.out.println("Select an option \n 1 - Add Employee \n 2 - View Employee \n 3 - Select an Employee \n" +
+                    "4 - Delete an Employee \n 5 - Exit");
+            Scanner input = new Scanner(System.in);
+            int n = input.nextInt();
 
-            ArrayList<employee> earr = new ArrayList<employee>();
+            if (n == 5) {
+                System.out.println("You are exiting from the program");
+                System.exit(0);
+            } else {
 
-            switch(n){
 
-                case 1 :
-                    System.out.println("Enter employee details");
-                    for(int i = 0; i < 5; i++){
+                switch (n) {
 
-                    employee ei = new employee();
-                    earr.add(ei);
+                    case 1:
+                        System.out.println("Enter employee details");
+                        for (int i = 0; i < 5; i++) {
 
-                    }
+                            employee ei = new employee();
+                            earr.add(ei);
+
+                        }
+                        break;
+
+                    case 2:
+                        System.out.println("Employee Details Showing");
+                        for (employee e : earr) {
+                            System.out.println(e);
+                        }
+                        break;
+                }
+
             }
-
         }
     }
 }
