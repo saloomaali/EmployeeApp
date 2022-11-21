ca25c752c2421b0dd23a5f024f94a78b18e97208
Salooma <saloomaali2000@gmail.com>
2022-11-21 15:47:48 +0530

    case for employee addition created

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,55 @@
-public class Main {
-
-    public class employee{
-        int code;
-        String name;
-        String designation;
-        double salary;
-        String companyName;
-        int phnNo;
-        String emailId;
-        employee(int code , String name, String designation, double salary, String companyName, int phnNo, String emailId){
-            this.code = code;
-            this.name = name;
-            this.designation = designation;
-            this. salary = salary;
-            this.companyName = companyName;
-            this.phnNo = phnNo;
-            this.emailId = emailId;
-
-        }
+import java.lang.runtime.SwitchBootstraps;
+import java.util.ArrayList;
+import java.util.Scanner;
+class employee{
+    int code;
+    String name;
+    String designation;
+    double salary;
+    String companyName;
+    long phnNo;
+    String emailId;
+    employee(){
+        Scanner obj = new Scanner(System.in);
+        code = obj.nextInt();
+        name = obj.next();
+        designation = obj.next();
+        salary = obj.nextDouble();
+        companyName = obj.next();
+        phnNo= obj.nextLong();
+        emailId = obj.next();
 
     }
+
+}
+public class Main {
+
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Select an option \n 1 - Add Employee \n 2 - View Employee \n 3 - Select an Employee \n" +
+                "4 - Delete an Employee \n 5 - Exit");
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+
+        if (n == 5){
+            System.out.println("You are exiting from the program");
+        }
+        else{
+
+            ArrayList<employee> earr = new ArrayList<employee>();
+
+            switch(n){
+
+                case 1 :
+                    System.out.println("Enter employee details");
+                    for(int i = 0; i < 5; i++){
+
+                    employee ei = new employee();
+                    earr.add(ei);
+
+                    }
+            }
+
+        }
     }
 }
